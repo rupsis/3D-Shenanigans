3b2ee86d98955b2d481ce94cd7085b422c98c45c
Nate Rupsis <C-Nathaniel.Rupsis@charter.com>
2022-02-15 14:01:09 -0600

    slight cleanup

--- RayTracingInOneWeekend/rust/target/debug/deps/ray_tracer-145c35b394529a49.d
+++
@@ -1,5 +0,0 @@
-/Users/P3024012/source/3D-Shenanigans/RayTracingInOneWeekend/rust/target/debug/deps/ray_tracer-145c35b394529a49: src/main.rs
-
-/Users/P3024012/source/3D-Shenanigans/RayTracingInOneWeekend/rust/target/debug/deps/ray_tracer-145c35b394529a49.d: src/main.rs
-
-src/main.rs:
