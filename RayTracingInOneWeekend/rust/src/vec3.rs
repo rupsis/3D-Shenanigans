f74603fa8882bcb35f053de65c3a0feb6a5fa36a
rupsis <C-Nathaniel.Rupsis@charter.com>
2022-02-16 10:49:44 -0600

    adding some more operators

--- RayTracingInOneWeekend/rust/src/vec3.rs
+++ RayTracingInOneWeekend/rust/src/vec3.rs
@@ -15,9 +15,27 @@ impl Vec3 {
     pub fn x(&self) -> f64 { self.0 }
     pub fn y(&self) -> f64 { self.1 }
     pub fn z(&self) -> f64 { self.2 }
+
+    pub fn length(&self) -> f64 {
+        f64::sqrt(self.length_squared())
+    }
+
+    pub fn length_squared(&self) -> f64 {
+        f64::powi(self.0, 2) + f64::powi(self.1, 2) + f64::powi(self.2, 2)
+    }
+
+    pub fn cross(&self, _rhs: Vec3) -> Vec3 {
+        Vec3::new(
+            self.y() * _rhs.z() - self.z() * _rhs.y(),
+            self.z() * _rhs.x() - self.x() * _rhs.z(),
+            self.x() * _rhs.y() - self.y() * _rhs.x()
+        )
+    }
 }
 
 // Overloading methods
+
+// + operation
 impl ops::Add<Vec3> for Vec3 {
     type Output = Self;
 
@@ -30,6 +48,20 @@ impl ops::Add<Vec3> for Vec3 {
     }
 }
 
+// - (subtract) operation
+impl ops::Sub<Vec3> for Vec3 {
+    type Output = Self;
+
+    fn sub(self, _rhs: Vec3) -> Vec3 {
+        Vec3::new(
+            self.x() - _rhs.x(),
+            self.y() - _rhs.y(),
+            self.z() - _rhs.z(),
+        )
+    }
+}
+
+// - (negate) operation 
 impl ops::Neg for Vec3 {
     type Output = Self;
 
@@ -39,6 +71,56 @@ impl ops::Neg for Vec3 {
             -self.y(),
             -self.z()
         )
+    }
+}
+
+// * (multiply) operation 
+impl ops::Mul<f64> for Vec3 {
+    type Output = Self;
+
+    fn mul(self, _rhs: f64) -> Vec3 {
+        Vec3::new(
+            self.x() * _rhs,
+            self.y() * _rhs,
+            self.z() * _rhs
+        )
+    }
+}
+
+// * (multiply) operation 
+impl ops::Div<f64> for Vec3 {
+    type Output = Self;
+
+    fn div(self, _rhs: f64) -> Vec3 {
+        self * (1.0/_rhs) 
+    }
+}
+
+// += operation
+impl ops::AddAssign for Vec3 {
+    fn add_assign(&mut self, _rhs: Self) {
+        *self = Self (
+            self.0 + _rhs.0,
+            self.1 + _rhs.1,
+            self.2 + _rhs.2
+        )
+    }
+}
+
+// *= operation
+impl ops::MulAssign<f64> for Vec3 {
+    fn mul_assign(&mut self, _rhs: f64) {
+        *self = Self (
+            self.0 * _rhs,
+            self.1 * _rhs,
+            self.2 * _rhs
+        )
+    }
+}
+
+impl ops::DivAssign<f64> for Vec3 {
+    fn div_assign(&mut self, _rhs: f64) {
+        *self *= 1.0/_rhs 
     }
 }
 
