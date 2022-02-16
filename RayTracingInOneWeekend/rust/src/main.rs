840218cf71a633aa2ae76a6b935d5bf6cfe34b8b
rupsis <nrupsis@gmail.com>
2022-02-16 10:53:48 -0600

    quick cleanup

--- RayTracingInOneWeekend/rust/src/main.rs
+++ RayTracingInOneWeekend/rust/src/main.rs
@@ -2,11 +2,10 @@ mod vec3;
 use std::io::{self, Write};
 
 fn main() {
+    let c = vec3::Color::new(0.0, 0.0, 0.0);
 
-    let c = vec3::Color::new(0.0,0.0,0.0);
+    println!("{}", c.x());
 
-println!("{}", c.x());
-    
     // Image
     let image_width = 256;
     let image_height = 256;
@@ -20,8 +19,8 @@ println!("{}", c.x());
         io::stdout().flush().unwrap();
 
         for i in 0..(image_width) {
-            let r = i as f64 / (image_width-1) as f64;
-            let g = j as f64 / (image_height-1) as f64;
+            let r = i as f64 / (image_width - 1) as f64;
+            let g = j as f64 / (image_height - 1) as f64;
             let b = 0.25;
 
             let ir = (255.999 * r) as i64;
--- RayTracingInOneWeekend/rust/src/vec3.rs
+++ RayTracingInOneWeekend/rust/src/vec3.rs
@@ -1,20 +1,25 @@
 use std::ops;
 
-
 pub struct Vec3(f64, f64, f64);
 
 impl Vec3 {
     pub fn default() -> Vec3 {
         Vec3(0.0, 0.0, 0.0)
-    } 
+    }
 
     pub fn new(e0: f64, e1: f64, e2: f64) -> Vec3 {
         Vec3(e0, e1, e2)
-    } 
+    }
 
-    pub fn x(&self) -> f64 { self.0 }
-    pub fn y(&self) -> f64 { self.1 }
-    pub fn z(&self) -> f64 { self.2 }
+    pub fn x(&self) -> f64 {
+        self.0
+    }
+    pub fn y(&self) -> f64 {
+        self.1
+    }
+    pub fn z(&self) -> f64 {
+        self.2
+    }
 
     pub fn length(&self) -> f64 {
         f64::sqrt(self.length_squared())
@@ -28,8 +33,12 @@ impl Vec3 {
         Vec3::new(
             self.y() * _rhs.z() - self.z() * _rhs.y(),
             self.z() * _rhs.x() - self.x() * _rhs.z(),
-            self.x() * _rhs.y() - self.y() * _rhs.x()
+            self.x() * _rhs.y() - self.y() * _rhs.x(),
         )
+    }
+
+    pub fn dot(&self, _rhs: Vec3) -> f64 {
+        self.x() * _rhs.x() + self.y() * _rhs.y() + self.z() * _rhs.z()
     }
 }
 
@@ -61,69 +70,52 @@ impl ops::Sub<Vec3> for Vec3 {
     }
 }
 
-// - (negate) operation 
+// - (negate) operation
 impl ops::Neg for Vec3 {
     type Output = Self;
 
     fn neg(self) -> Vec3 {
-        Vec3::new(
-            -self.x(),
-            -self.y(),
-            -self.z()
-        )
+        Vec3::new(-self.x(), -self.y(), -self.z())
     }
 }
 
-// * (multiply) operation 
+// * (multiply) operation
 impl ops::Mul<f64> for Vec3 {
     type Output = Self;
 
     fn mul(self, _rhs: f64) -> Vec3 {
-        Vec3::new(
-            self.x() * _rhs,
-            self.y() * _rhs,
-            self.z() * _rhs
-        )
+        Vec3::new(self.x() * _rhs, self.y() * _rhs, self.z() * _rhs)
     }
 }
 
-// * (multiply) operation 
+// * (multiply) operation
 impl ops::Div<f64> for Vec3 {
     type Output = Self;
 
     fn div(self, _rhs: f64) -> Vec3 {
-        self * (1.0/_rhs) 
+        self * (1.0 / _rhs)
     }
 }
 
 // += operation
 impl ops::AddAssign for Vec3 {
     fn add_assign(&mut self, _rhs: Self) {
-        *self = Self (
-            self.0 + _rhs.0,
-            self.1 + _rhs.1,
-            self.2 + _rhs.2
-        )
+        *self = Self(self.0 + _rhs.0, self.1 + _rhs.1, self.2 + _rhs.2)
     }
 }
 
 // *= operation
 impl ops::MulAssign<f64> for Vec3 {
     fn mul_assign(&mut self, _rhs: f64) {
-        *self = Self (
-            self.0 * _rhs,
-            self.1 * _rhs,
-            self.2 * _rhs
-        )
+        *self = Self(self.0 * _rhs, self.1 * _rhs, self.2 * _rhs)
     }
 }
 
 impl ops::DivAssign<f64> for Vec3 {
     fn div_assign(&mut self, _rhs: f64) {
-        *self *= 1.0/_rhs 
+        *self *= 1.0 / _rhs
     }
 }
-
 
 // Type aliases
 pub use Vec3 as Point3;
@@ -131,7 +123,13 @@ pub use Vec3 as Color;
 
 // Color utility methods
 impl Color {
-    pub fn r(&self) -> f64 { self.0 }
-    pub fn g(&self) -> f64 { self.1 }
-    pub fn b(&self) -> f64 { self.2 }
+    pub fn r(&self) -> f64 {
+        self.0
+    }
+    pub fn g(&self) -> f64 {
+        self.1
+    }
+    pub fn b(&self) -> f64 {
+        self.2
+    }
 }
